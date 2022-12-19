54d4582debbed5f37712a85a6e79ec255aa1f4ea
dk <digambarbkarande1997@gmail.com>
2022-12-19 14:26:52 +0530

    first commit

---
+++ src/main/java/mvnPomclass/Neostoxsighnin.java
@@ -0,0 +1,36 @@
+package mvnPomclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Neostoxsighnin {
+
+	
+	
+	@FindBy(xpath="(//a[text()='Sign In'])[1]") private WebElement sighninbutton;
+	
+	//edited project
+	
+	public Neostoxsighnin(WebDriver driver)
+	
+	{
+		PageFactory.initElements(driver,this);
+	}
+	
+	public void sighnbutton()
+	{
+		sighninbutton.click();
+	}
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
